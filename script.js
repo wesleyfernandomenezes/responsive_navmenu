77e326996025b36e486afc7b81f555c03b77dda5
Wesley Menezes <38982458+wesleyfernandomenezes@users.noreply.github.com>
2020-10-09 17:45:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,25 @@ function start () {
 /* NAVMENU */
 function navMenu () {
     const hamburgerMenu = document.querySelector('.hamburger-menu')
-    const navMenu = document.querySelector('.nav-menu')
+    const navMenu =  document.querySelector('.nav-menu')
+    const navLink = document.querySelectorAll('.nav-link')
 
+    // IT CHANGES THE HAMBURGER BUTTON (TOGGLE = SWITCH)
     hamburgerMenu.addEventListener('click', function () {
+        toggle ()
+    })
+
+    // WHEN YOU CLICK, THE MENU CLOSES
+    navLink.forEach((item) => {
+        item.addEventListener('click', function () {
+            if (hamburgerMenu.classList.contains('when-active')) {
+                toggle ()
+            }
+        })
+    })
+
+    function toggle () {
         hamburgerMenu.classList.toggle('when-active')
         navMenu.classList.toggle('active')
-    })
-}
+    }
+}}
